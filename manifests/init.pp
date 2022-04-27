c81aa917e2135db327e9f846031bd2b5eb537715
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2022-04-27 17:17:16 +0200

    Initial support for Rocky

--- manifests/init.pp
+++ manifests/init.pp
@@ -54,8 +54,8 @@ inherits bind::params
     }
 
     case $::operatingsystem {
-        'debian', 'ubuntu':         { include bind::common::debian }
-        'redhat', 'fedora', 'centos': { include bind::common::redhat }
+        'debian', 'ubuntu':                    { include bind::common::debian }
+        'redhat', 'fedora', 'centos', 'rocky': { include bind::common::redhat }
         default: {
             fail("Module ${::module_name} is not supported on ${::operatingsystem}")
         }
